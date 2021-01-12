24c03de8f2dd2442713c56e3f0eabe3a2b66455e
Daniyal_Akram_07 <77106224+daniyal-akram@users.noreply.github.com>
2021-01-12 11:40:06 +0500

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int length,width;
+ cout<<"entre the length :"<<endl;
+ cin>>length;
+ cout<<"entre the width :"<<endl;
+ cin>>width;
+ cout<<"area of reactangle="<<length*width<<endl;
+ cout<<"parameter pf ractangle="<<(length*2)+(width*2);
+ return 0;
+
+}
